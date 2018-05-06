a1c18164d25abf9900edfd4813db22d77fc4a04b
chriswhite <chriswhite5918@gmail.com>
2018-05-06 22:11:58 +0800

    Added two btn to Home.vue

--- my-vuetify/src/components/Home.vue
+++ my-vuetify/src/components/Home.vue
@@ -1,7 +1,18 @@
 <template>
-    <div>
-        <p>The Home Page</p>
-    </div>
+    <v-container>
+        <v-layout row wrap>
+            <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+                <v-btn large router to="/meetups" color="info">
+                    Explore Meetups
+                </v-btn>
+            </v-flex>
+            <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+                <v-btn large router to="/meetup/new" color="info">
+                    Organize Meetup
+                </v-btn>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
